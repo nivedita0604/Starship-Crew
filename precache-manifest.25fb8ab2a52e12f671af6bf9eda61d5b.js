0c3d02e77ffcacd62f9ba37df991788e7d1e23e7
nivedita0604 <54851166+nivedita0604@users.noreply.github.com>
2020-07-24 22:05:29 +0530

    Updates

---
+++ precache-manifest.25fb8ab2a52e12f671af6bf9eda61d5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf03f58c190a47dfbef61e735bfca01",
+    "url": "/GeshikaSingh/Starship/index.html"
+  },
+  {
+    "revision": "f07f3c7cba0444525715",
+    "url": "/GeshikaSingh/Starship/static/css/main.aaadda0a.chunk.css"
+  },
+  {
+    "revision": "6701e78002780f0a5996",
+    "url": "/GeshikaSingh/Starship/static/js/2.a02577f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GeshikaSingh/Starship/static/js/2.a02577f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07f3c7cba0444525715",
+    "url": "/GeshikaSingh/Starship/static/js/main.a451f25c.chunk.js"
+  },
+  {
+    "revision": "55ef094e24f06fcb0d08",
+    "url": "/GeshikaSingh/Starship/static/js/runtime-main.2f4c8e95.js"
+  },
+  {
+    "revision": "25a7d55c1961500ba79b534a69a8b1f4",
+    "url": "/GeshikaSingh/Starship/static/media/spaces.25a7d55c.png"
+  }
+]);
